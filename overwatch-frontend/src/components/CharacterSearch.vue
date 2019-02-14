66d48119923b9392c7b4a19798baa076cb124792
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-02-14 09:50:40 -0700

    change

--- overwatch-frontend/src/components/CharacterSearch.vue
+++ overwatch-frontend/src/components/CharacterSearch.vue
@@ -24,7 +24,7 @@ export default {
   data() {
     return {
       images: ['https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/4/46/Hanamura_concept.jpg/800px-Hanamura_concept.jpg?version=cba0e20ddb6e4d0a188668500b8bb7ab','https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/d/de/Anubis_concept.jpg/800px-Anubis_concept.jpg?version=01cc8d241e63fa2721da953444149630']
-    };
+    }
   },
   methods: {
     loadCharacter() {
